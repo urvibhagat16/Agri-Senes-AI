90b3baf3924449debd4df8c0dce0451a5709d351
Urvi <urvibhagat3222@gmail.com>
2026-07-09 23:14:33 +0530

    Added project structure

---
+++ src/lib/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabase = createClient("https://locgobvapibsdihpceem.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvY2dvYnZhcGlic2RpaHBjZWVtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzYwMzc5OCwiZXhwIjoyMDk5MTc5Nzk4fQ.7Otyrff6TPqVWc6sWLGte72RGgQuB6VtPZij7L-QYB0")
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
+
